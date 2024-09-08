8dc4a4a58424bd7d844f7bf12156d3ffac4f3530
Luigi <luigicumaru@gmail.com>
2024-09-08 13:11:40 -0300

    Ajuste Javascript

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const button = document.getElementById('button__not');
 
+button.addEventListener('click', () => {
+    const widthWindow = window.innerWidth;
+    const heightWindow = window.innerHeight;
+
+    const newPositionX = Math.random() * (widthWindow - button.offsetWidth) + 10;
+    const newPositionY = Math.random() * (heightWindow - button.offsetHeight) + 10;
+
+    button.style.position = 'absolute';
+    button.style.left = newPositionX + 'px';
+    button.style.top = newPositionY + 'px';
+})
+
 button.addEventListener('mouseover', () => {
     const widthWindow = window.innerWidth;
     const heightWindow = window.innerHeight;
