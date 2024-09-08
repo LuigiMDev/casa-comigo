8545ccf0e1de0704f6320998d757c953d63edbab
Luigi <luigicumaru@gmail.com>
2024-09-08 13:08:06 -0300

    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const button = document.getElementById('button__not');
+
+button.addEventListener('mouseover', () => {
+    const widthWindow = window.innerWidth;
+    const heightWindow = window.innerHeight;
+
+    const newPositionX = Math.random() * (widthWindow - button.offsetWidth) + 10;
+    const newPositionY = Math.random() * (heightWindow - button.offsetHeight) + 10;
+
+    button.style.position = 'absolute';
+    button.style.left = newPositionX + 'px';
+    button.style.top = newPositionY + 'px';
+})
